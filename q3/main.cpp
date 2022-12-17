05c91780d28d1fdf11cff43a5ad6480da5bddff3
Brad <bmasciotra@my.bcit.ca>
2022-12-17 10:20:53 -0800

    have to account for same letters in different indices

--- q3/main.cpp
+++ q3/main.cpp
@@ -4,57 +4,102 @@
 #include <map>
 #include <cstring>
 #include <algorithm>
+#include <vector>;
 
 using namespace std;
 
-class Solution{
+class Solution {
 
 public:
-    Solution(){};
+    Solution() {};
+
     bool isIsomorphic(string s, string t) {
 
-        map<char,int> letterIndicesS1;
-        map<char,int> letterIndicesS2;
-
-        char str1cpy[s.length() + 1];
-        char str2cpy[t.length() + 1];
+        //map<char, int> letterIndicesS1, letterIndicesS2;
+        vector<pair<char, int>> letterIndicesS1, letterIndicesS2;
+        char str1cpy[s.length() + 1], str2cpy[t.length() + 1];
+        bool isomorphic = true;
 
         strcpy(str1cpy, s.c_str());
         strcpy(str2cpy, t.c_str());
 
-
-        cout << "copy: " << str1cpy << endl;
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
 
             char letter = str1cpy[i];
             char second_letter = str2cpy[i];
 
+            bool found1 = false;
+            bool found2 = false;
 
-            for(int y = 0; y < letterIndicesS1.size();y++){
-
-                pair<char,int> keyVal = letterIndicesS1.;
-                if(letter == key.first){
-                    key.second++;
-                }else{
-                    cout << "here" << endl;
-                    letterIndicesS1.insert(make_pair(letter,1));
+            if (letterIndicesS1.empty() && letterIndicesS2.empty()) {
+                letterIndicesS1.emplace_back(letter, 1);
+                letterIndicesS2.emplace_back(second_letter, 1);
+            } else {
+                if (letter == letterIndicesS1[letterIndicesS1.size() - 1].first) {
+                    found1 = true;
+                    letterIndicesS1[letterIndicesS1.size() - 1].second++;
                 }
-                cout << key.first <<endl;
-            });
 
+                if (second_letter == letterIndicesS2[letterIndicesS2.size() - 1].first) {
+                    found2 = true;
+                    letterIndicesS2[letterIndicesS2.size() - 1].second++;
+                }
 
+                if (!found1) {
+                    letterIndicesS1.emplace_back(letter, 1);
+                }
+
+                if (!found2) {
+                    letterIndicesS2.emplace_back(second_letter, 1);
+                }
+            }
         }
 
 
-        return true;
+        cout << "Map 1" << endl;
+        for_each(letterIndicesS1.begin(), letterIndicesS1.end(), [&](pair<const char, int> key) mutable -> void {
+            cout << "KEY: " << key.first << " VAL: " << key.second << endl;
+        });
+
+        cout << "Map 2" << endl;
+        for_each(letterIndicesS2.begin(), letterIndicesS2.end(), [&](pair<const char, int> key) mutable -> void {
+            cout << "KEY: " << key.first << " VAL: " << key.second << endl;
+        });
+
+        auto it2 = letterIndicesS2.begin();
+        for (auto it1 = letterIndicesS1.begin(); it1 != letterIndicesS1.end(); it1++) {
+
+            for (it2; it2 != letterIndicesS2.end(); it2++) {
+
+                if (it1->second == it2->second) {
+                    break;
+                } else {
+                    cout << "WRONG: " << it1->first << " :" << it1->second << endl;
+                    cout << "WRONG: " << it2->first << " :" << it2->second << endl;
+                    isomorphic = false;
+                    break;
+                }
+            }
+
+            it2++;
+            if (!isomorphic) {
+                break;
+            }
+
+        }
+
+        return isomorphic;
     }
 };
 
-int main(){
+int main() {
 
 
-    Solution solution =  Solution{};
-    solution.isIsomorphic("egg" , "add");
+    Solution solution = Solution{};
+    cout << solution.isIsomorphic("foo", "bar") << endl;
+
+    Solution solution2 = Solution{};
+    cout << solution2.isIsomorphic("egg", "add") << endl;
 
     return 0;
 }
